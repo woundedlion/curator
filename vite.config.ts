1fd4e2d5553cec47c7a45b89c4a36a05f5169473
Gabriel Levy <gabelevy@gmail.com>
2026-05-26 13:58:08 -0700

    make pages deployment work

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/curator/" : "/",
   plugins: [react()],
   server: { port: 5173 },
   worker: {
     format: "es",
   },
-});
+}));
